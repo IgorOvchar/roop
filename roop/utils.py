dbf33d326561af718fa2321ebf6e1b430b55974b
Eugene Lis <97803837+Obnovlator3000@users.noreply.github.com>
2023-06-05 00:25:51 +0300

    Update utils.py
    Теперь должно работать и без звука, спасибо https://pikabu.ru/@Xan666

--- roop/utils.py
+++ roop/utils.py
@@ -48,7 +48,7 @@ def add_audio(output_dir, target_path, video, keep_frames, output_file):
     video_name = os.path.splitext(video)[0]
     save_to = output_file if output_file else output_dir + "/swapped-" + video_name + ".mp4"
     save_to_ff, output_dir_ff = path(save_to), path(output_dir)
-    os.system(f'ffmpeg -i "{output_dir_ff}{sep}output.mp4" -i "{output_dir_ff}{sep}{video}" -c:v copy -map 0:v:0 -map 1:a:0 -y "{save_to_ff}" -loglevel error')
+    os.system(f'ffmpeg -i "{output_dir_ff}{sep}output.mp4" -i "{output_dir_ff}{sep}{video}" -c:v copy -map 0:v:0 -map 1:a:0? -y "{save_to_ff}" -loglevel error')
     if not os.path.isfile(save_to):
         shutil.move(output_dir + "/output.mp4", save_to)
     if not keep_frames:
